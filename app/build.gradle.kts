a5eb009bffa4e43171f4744eed40e3e872056585
nothtl <tingli.hu05@gmail.com>
2026-06-24 13:22:06 -0400

    version 0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 15
-        versionName = "0.7.1"
+        versionName = "0.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
